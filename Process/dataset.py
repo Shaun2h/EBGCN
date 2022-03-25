make it work with pheme. No more guarantees for Twitter 15/16, fix their logging to actually be overall f1 and not average batch f1.

Process/dataset.py
@@ -2,13 +2,60 @@ import os
 import numpy as np
 import torch
 import random
+import pickle
+import pprint
+import json
 from torch.utils.data import Dataset
 from torch_geometric.data import Data
+from transformers import BertTokenizer, BertModel
 
+
+# def reset_models():
+global device
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+global Bert_Tokeniser 
+Bert_Tokeniser =  BertTokenizer.from_pretrained("bert-base-multilingual-uncased")
+global Bert_Embed 
+Bert_Embed = BertModel.from_pretrained("bert-base-multilingual-uncased").to(device)
+Bert_Embed.resize_token_embeddings(len(Bert_Tokeniser))
+# reset_models()
+
+def collate_fn(data):
+    return data
+
+def get_td_bu_edges(tddroprate,budroprate,edge_index_matrix):
+    if tddroprate > 0:
+        row = list(edge_index_matrix[0])
+        col = list(edge_index_matrix[1])
+        length = len(row)
+        # poslist = random.sample(range(length), int(length * (1 - self.tddroprate)))
+        poslist = random.sample(range(length), int(length * (1)))
+        poslist = sorted(poslist)
+        row = list(np.array(row)[poslist])
+        col = list(np.array(col)[poslist])
+        new_edgeindex = [row, col]
+    else:
+        new_edgeindex = edge_index_matrix
+
+    burow = list(edge_index_matrix[1])
+    bucol = list(edge_index_matrix[0])
+    if budroprate > 0:
+        length = len(burow)
+        # poslist = random.sample(range(length), int(length * (1 - self.budroprate)))
+        poslist = random.sample(range(length), int(length * (1)))
+        poslist = sorted(poslist)
+        row = list(np.array(burow)[poslist])
+        col = list(np.array(bucol)[poslist])
+        bunew_edgeindex = [row, col]
+    else:
+        bunew_edgeindex = [burow,bucol]
+    return new_edgeindex, bunew_edgeindex
 
 class BiGraphDataset(Dataset):
     def __init__(self, fold_x, treeDic,lower=2, upper=100000, tddroprate=0,budroprate=0,
                  data_path=os.path.join('..','..', 'data', 'Weibograph')):
+        
+        self.is_pheme_pointer = False
         self.fold_x = list(filter(lambda id: id in treeDic and len(treeDic[id]) >= lower and len(treeDic[id]) <= upper, fold_x))
         self.treeDic = treeDic
         self.data_path = data_path
@@ -22,34 +69,278 @@ class BiGraphDataset(Dataset):
         id =self.fold_x[index]
         data=np.load(os.path.join(self.data_path, id + ".npz"), allow_pickle=True)
         edgeindex = data['edgeindex']
-        if self.tddroprate > 0:
-            row = list(edgeindex[0])
-            col = list(edgeindex[1])
-            length = len(row)
-            poslist = random.sample(range(length), int(length * (1 - self.tddroprate)))
-            poslist = sorted(poslist)
-            row = list(np.array(row)[poslist])
-            col = list(np.array(col)[poslist])
-            new_edgeindex = [row, col]
+        new_edgeindex, bunew_edgeindex = get_td_bu_edges(self.tddroprate,self.budroprate,edgeindex)
+        output_data = Data(x=torch.tensor(data['x'],dtype=torch.float32),
+                edge_index=torch.LongTensor(new_edgeindex),BU_edge_index=torch.LongTensor(bunew_edgeindex),
+                y=torch.LongTensor([int(data['y'])]), root=torch.LongTensor(data['root']),
+                rootindex=torch.LongTensor([int(data['rootindex'])]))
+
+        output_data.num_nodes = output_data.x.size(0) # prevents a torch scatter error.
+        return output_data
+
+
+class bigraph_dataset_PHEME(Dataset):
+    def __init__(self, fold_x, treeDic, tddroprate=0,budroprate=0, picklefear=True):
+        # Pheme loader HERE.
+        self.tddroprate = tddroprate
+        self.budroprate = budroprate
+        self.picklefear = picklefear
+        if not picklefear: # Use Pickle (NOT SUITABLE FOR NON SELF USE. SERVER USERS BEWARE.)
+            if not os.path.exists("overall_data_dump.pck"): # if your comp crashed, we will load what we've got. If not, we initialise.
+                with open("overall_data_dump.pck","wb") as datadump:
+                    pickle.dump({},datadump,pickle.HIGHEST_PROTOCOL)
+                alt_dump_holder_data = {}
+            else:
+                with open("overall_data_dump.pck","rb") as datadump: # CRASH PREVENTION.
+                    alt_dump_holder_data = pickle.load(datadump)
+
+            with open("phemethreaddump.json","rb") as dumpfile:
+                allthreads = json.load(dumpfile)
+                
+                
+            print("Tree extraction - PHEME. Will take a Massive amount of time: Extracts ALL trees at once. No need to do per dataload unlike theirs.")
+            print("Drawback: Costs more memory, and really really long initial wait time.")
+            print("Picks up where it 'crashed off', if it hasn't finished compiling, or sees a new thing.")
+
+            tempytokenizer = Bert_Tokeniser
+            tempymodel = Bert_Embed
+            donecounter = 0
+            for thread in allthreads:
+                threadtextlist,tree,rootlabel,source_id = thread
+
+                if not source_id in fold_x: # Lazy loading, in the sense that if this is not used this time, we won't update.
+                    continue
+                if source_id in alt_dump_holder_data: # we're just checking to "update" our database.
+                    continue
+                alt_data = self.process_thread(thread,tempytokenizer,tempymodel,returnalt=True)
+                alt_dump_holder_data[source_id] = alt_data
+                with open("overall_data_dump.pck","wb") as datadump: # shove the update in.
+                    pickle.dump(alt_dump_holder_data,datadump,pickle.HIGHEST_PROTOCOL)
+                donecounter+=1
+
+                
+            # All possible points in this dataset's fold have been processed before/recently processed
+            self.data_PHEME = []
+            with open("overall_data_dump.pck","rb") as datadump:
+                alt_dump_holder_data = pickle.load(datadump)
+            for source_id in fold_x:
+                relevantdict = alt_dump_holder_data[source_id]
+                forceddict = {} # i'm paranoid about shallow copies... the memory cost is negligble.
+                forceddict["edgeindex"] = np.array(relevantdict["edgeindex"])
+                forceddict["y"] = relevantdict["y"]
+                forceddict["x"] = torch.tensor(np.array(relevantdict["x"]))
+                forceddict["rootindex"] = relevantdict["rootindex"]
+                forceddict["root"] = torch.tensor(np.array(relevantdict["root"]))
+                self.data_PHEME.append(forceddict)
+                # ["tweettext","tweetid","authid"]
+            print("Completed Loading Tree - (ALL POSSIBLE)")
+            
+            
+        else: # Do not use pickle. Regular mode. Loads on demand.
+            self.tokeniser = Bert_Tokeniser
+            self.model = Bert_Embed
+            with open("phemethreaddump.json","rb") as dumpfile:
+                loaded_threads = json.load(dumpfile)
+                
+            self.allthreads = {}
+            
+            for thread in loaded_threads:
+                threadtextlist,tree,rootlabel,source_id = thread
+                if source_id in fold_x:
+                    self.allthreads[source_id] = thread
+                    
+            self.fold_x = fold_x
+            print("Skipping Pre-Tree Load, ignoring the use of pickle.")
+
+    def __len__(self):
+        if not self.picklefear:
+            return len(self.data_PHEME)
         else:
-            new_edgeindex = edgeindex
+            return len(self.fold_x)
 
-        burow = list(edgeindex[1])
-        bucol = list(edgeindex[0])
-        if self.budroprate > 0:
-            length = len(burow)
-            poslist = random.sample(range(length), int(length * (1 - self.budroprate)))
-            poslist = sorted(poslist)
-            row = list(np.array(burow)[poslist])
-            col = list(np.array(bucol)[poslist])
-            bunew_edgeindex = [row, col]
+    def process_thread(self,somethread,tempytokenizer,tempymodel,returnalt=False):
+        """
+        Process a Thread in Shaun's Format to their required format so we can use a similar loading func like they do.
+        """
+        threadtextlist,tree,rootlabel,source_id = somethread
+            # break
+            # if donecounter>400:
+                # break
+            #break # 100 is enough for the test... 
+            # noteworthy is that their splits are for whole dataset splits, not like dataset into 5 splits then train test each.
+        threaddict = {}
+        pointerdict = {}
+        counter = 0 
+        for text in threadtextlist: # for easier reference.
+            threaddict[text[1]] = text[0] # ,text[2] #only append text, ignoring authors and ids. Can be edited later... # note here.
+                                        # keep in mind you must edit collation function OR/AND the dataset class after.
+            pointerdict[text[1]] = counter #tweet id
+            counter+=1
+            # ["tweettext","tweetid","authid"]
+
+            # aim is to create an edge matrix like their preprocessing.
+        # pprint.pprint(source_id)
+        # pprint.pprint(threaddict)
+        # print(len(list(threaddict.keys())),counter-1)
+        # input()
+        fromrow = []
+        torow = []
+        for sender in tree:
+            for target in tree[sender]:
+                fromrow.append(pointerdict[sender])
+                torow.append(pointerdict[target])
+        # edge matrix is created... for those that lack edges.. i'll raise this later in discussion.
+        invertedpointer = dict(map(reversed, pointerdict.items()))
+        allinputs_list = []  # allinputs_list acts as data["x"]
+        for numbered in range(len(list(pointerdict.keys()))):
+            allinputs_list.append(threaddict[invertedpointer[numbered]])
+        # print(threaddict)
+        data ={}
+        alt_data = {}
+        # print(allinputs_list)
+        # print("Reattempting")
+        with torch.no_grad():
+            # try:
+                allinputs_list = tempytokenizer(list(allinputs_list), padding="max_length", max_length=256, truncation=True, return_tensors="pt")
+            # torch.set_printoptions(threshold = 1000000)
+            # print(allinputs_list.input_ids)
+            # print(len(allinputs_list.input_ids),len(list(threaddict.keys())))
+                allinputs_list.to(device)
+                allinputs_list = tempymodel(**allinputs_list)
+                allinputs_list = allinputs_list[0].cpu()#.last_hidden_state
+                allinputs_list = allinputs_list.cpu()
+            # except RuntimeError as e:
+                # print(e)
+                # trashtoken =  BertTokenizer.from_pretrained("bert-base-multilingual-uncased")
+                # trashembed = BertModel.from_pretrained("bert-base-multilingual-uncased")
+                # trashembed.resize_token_embeddings(len(trashtoken))
+                # allinputs_list = trashtoken(list(allinputs_list), padding="max_length", max_length=256, truncation=True, return_tensors="pt")
+                # allinputs_list = trashembed(**allinputs_list)
+                # allinputs_list = allinputs_list.last_hidden_state
+                # print("Failcase") 
+                # input()
+                # Unknown why some of the data just errors out.
+                # return False,False
+                
+        
+        # Note max source length is 512 which is the max for this bert model
+        # but.. because of my gpu being small, it's 256. Anything above is TRUNCATED.
+        
+        with torch.no_grad():
+            data['root'] = tempymodel(**tempytokenizer(threaddict[source_id], padding="max_length", max_length=256, truncation=True, return_tensors="pt").to(device))[0].cpu()#.last_hidden_state.cpu()
+            
+        
+        
+        
+        if returnalt:
+            alt_data["rootindex"] = pointerdict[source_id]
+            alt_data["x"] = allinputs_list.detach().cpu().numpy().tolist()
+            alt_data["root"] = data["root"].detach().cpu().numpy().tolist()
+            alt_data["y"] = rootlabel[0]
+            alt_data["edgeindex"] = np.array([fromrow,torow]).tolist() # imitating their dataloading method.
+            return alt_data
         else:
-            bunew_edgeindex = [burow,bucol]
-        return Data(x=torch.tensor(data['x'],dtype=torch.float32),
-                    edge_index=torch.LongTensor(new_edgeindex),
-                    BU_edge_index=torch.LongTensor(bunew_edgeindex),
-                    y=torch.LongTensor([int(data['y'])]),
-                    root=torch.LongTensor(data['root']),
-                    rootindex=torch.LongTensor([int(data['rootindex'])]))
+            data["rootindex"] = pointerdict[source_id]
+            data["x"] = allinputs_list # you also need to convert this to a tensor.
+            data["y"] = rootlabel[0]
+            data["edgeindex"] = np.array([fromrow,torow]) # imitating their dataloading method.
+            # print(rootlabel[0])  # rootlabel[0] acts as data["y"]
+            # 0 = nonrumour, 1 = rumour
+            return data
+        # False "data" has been created. same structure as whatever they had.
+        
+        
 
+    def roundback(self,index):
+        """
+        backtrack and find out more information on the original data/tree etc that constituted this particular index.
+        
+        Return format:
+        threadtextlist,tree,rootlabel,source_id
+        threadtextlist -> [ ["tweettext","tweetid","authid"], ["tweettext","tweetid","authid"] ....  ]
+        tree -> {id1:[<CHILDREN>], id2:[<CHILDREN>] ...  }   # note that it should be strings for the id iirc
+        rootlabel -> 0 or 1,   -->    0 = nonrumour, 1 = rumour
+        source_id -> the original nodes' id
+        """
+        threadtextlist,tree,rootlabel,source_id = self.data_PHEME[index]
+        return threadtextlist,tree,rootlabel,source_id
+        
 
+    def check_violations(self,data_inp):
+        """
+        Given a data format that similar to their format, check for a edge matrix indexing violation versus total number of nodes.
+        """
+        try:
+            if torch.max(torch.tensor(data_inp["edgeindex"][0]),dim=0)[0]>=data_inp["x"].shape[0]:
+                print("(FROM) VIOLATION IN DATASET.",torch.max(torch.tensor(data_inp["edgeindex"][0]),dim=0)[0],"      VS      ", data_inp["x"].shape[0])
+                print("There are more reported nodes in the edge matrix compared to the ACTUAL data matrix.")
+                print("In my case, it was due to a node pointing to itself as the parent in the dataset, causing a double report of a node.")
+                print("i.e the SAME NODE is reported TWO TIMES!")
+                print("won't crash on cpu for some reason. BUT EVENTUALLY RESULTS IN CUDA DEVICE ASSERTION ERROR DUE TO INDEXING.")
+        except IndexError:
+            pass # can't violate if dim 0 is EMPTY.
+        try:
+            if torch.max(torch.tensor(data_inp["edgeindex"][1]),dim=0)[0]>=data_inp["x"].shape[0]:
+                print("(TO) VIOLATION IN DATASET",torch.max(torch.tensor(data_inp["edgeindex"][1]),dim=0)[0],"      VS      ", data_inp["x"].shape[0])
+                print("There are more reported nodes in the edge matrix compared to the ACTUAL data matrix.")
+                print("In my case, it was due to a node pointing to itself as the parent in the dataset, causing a double report of a node.")
+                print("i.e the SAME NODE is reported TWO TIMES!")
+                print("won't crash on cpu for some reason. BUT EVENTUALLY RESULTS IN CUDA DEVICE ASSERTION ERROR DUE TO INDEXING.")
+        except IndexError:
+            pass # can't violate if dim 0 is EMPTY.
+    def __getitem__(self, index):
+        """
+        get item via index. Mimic verison.
+        """
+
+        if not self.picklefear:
+            data = self.data_PHEME[index] # data was already preloaded.
+        else:
+            data = self.process_thread(self.allthreads[self.fold_x[index]],self.tokeniser,self.model)
+            
+        # EXPERIMENTS TO SEE THE DATA FORMAT SO WE CAN MIMIC.
+        # import sys
+        # np.set_printoptions(threshold=sys.maxsize)
+        # print("root index: ",data['rootindex'])
+        # print(data['root']) # always holds the root node's vector. however or whatever you encode it via.
+        # print("Root shape:",data['root'].shape)
+        # print(data['x'])
+        # print("x shape:",data['x'].shape)
+        # print(data['edgeindex'])
+        # print("edge index shape: ",data['edgeindex'].shape)
+        # edge index = edge matrix
+        # row 1 FROM
+        # row 2 TO
+        # print("Label:",data['y'])
+        # 0 = ['news', 'non-rumor'], 
+        # 1 = ['false'], 
+        # 2 = ['true'], 
+        # 3 = ['unverified']
+        
+        edgeindex = data['edgeindex']
+        self.check_violations(data) # for your information.
+
+        # print(data["x"].shape)
+        new_edgeindex, bunew_edgeindex = get_td_bu_edges(self.tddroprate,self.budroprate,edgeindex)
+
+        # SHAPE INFORMATION FOR MODEL SIZING/SHAPING:
+        
+        # print(torch.LongTensor(bunew_edgeindex).shape)
+        # print(torch.LongTensor(new_edgeindex).shape)
+        # print(data["root"].shape)
+        # print(data["x"].shape) #you need to flatten and CHANGE the model shape in order to fit...
+        # print("overall number of tweets shape:",data["x"].shape)
+        # print("original:",np.array(self.data_PHEME[index]["edgeindex"]))
+        # print("BUEdgeIndex:",torch.LongTensor(new_edgeindex))
+        # print("TDEdgeIndex:",torch.LongTensor(bunew_edgeindex))
+        
+        output_data = Data(x=data['x'].reshape(data["x"].shape[0],-1),
+                edge_index=torch.LongTensor(new_edgeindex),BU_edge_index=torch.LongTensor(bunew_edgeindex),
+                y=torch.LongTensor([int(data['y'])]), root=data['root'].reshape(data["root"].shape[0],-1),
+                rootindex=torch.LongTensor([int(data['rootindex'])]))
+        output_data.num_nodes = output_data.x.size(0) # prevents a torch scatter error.
+        output_data._silent_indexcheck = index # breadcrumb for finding the origin.
+        return output_data
+
+    
+        

main_ebgcn.py
@@ -1,5 +1,7 @@
 import sys, os
 import argparse
+import json
+import pprint
 from time import time
 sys.path.append(os.getcwd())
 from Process.process import *
@@ -13,7 +15,7 @@ from tools.evaluate import *
 from model.EBGCN import EBGCN
 
 
-def train_model(treeDic, x_test, x_train, args, iter):
+def train_model(treeDic, x_test, x_train, args, iter,commentary=""):
     model = EBGCN(args).to(args.device)
     TD_params = list(map(id, model.TDrumorGCN.conv1.parameters()))
     TD_params += list(map(id, model.TDrumorGCN.conv2.parameters()))
@@ -36,7 +38,7 @@ def train_model(treeDic, x_test, x_train, args, iter):
     early_stopping = EarlyStopping(patience=args.patience, verbose=True)
 
     for epoch in range(args.n_epochs):
-        traindata_list, testdata_list = loadData(args.datasetname, treeDic, x_train, x_test, args.TDdroprate, args.BUdroprate)
+        traindata_list, testdata_list = loadBiData(args.datasetname, treeDic, x_train, x_test, args.TDdroprate, args.BUdroprate)
         train_loader = DataLoader(traindata_list, batch_size=args.batchsize, shuffle=True, num_workers=args.num_workers)
         test_loader = DataLoader(testdata_list, batch_size=args.batchsize, shuffle=False, num_workers=args.num_workers)
         avg_loss = []
@@ -44,6 +46,7 @@ def train_model(treeDic, x_test, x_train, args, iter):
         batch_idx = 0
         for Batch_data in train_loader:
             Batch_data.to(args.device)
+            # print(Batch_data)
             out_labels,  TD_edge_loss, BU_edge_loss = model(Batch_data)
             loss = F.nll_loss(out_labels, Batch_data.y)
             if TD_edge_loss is not None:
@@ -76,6 +79,8 @@ def train_model(treeDic, x_test, x_train, args, iter):
         temp_val_Acc3, temp_val_Prec3, temp_val_Recll3, temp_val_F3, \
         temp_val_Acc4, temp_val_Prec4, temp_val_Recll4, temp_val_F4 = [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
         model.eval()
+        rawcounts = {1:{"TP":0,"FP":0,"FN":0,"TN":0},2:{"TP":0,"FP":0,"FN":0,"TN":0},3:{"TP":0,"FP":0,"FN":0,"TN":0},4:{"TP":0,"FP":0,"FN":0,"TN":0}}
+
         for Batch_data in test_loader:
             Batch_data.to(args.device)
             val_out, _, _ = model(Batch_data)
@@ -84,7 +89,7 @@ def train_model(treeDic, x_test, x_train, args, iter):
             _, val_pred = val_out.max(dim=1)
             correct = val_pred.eq(Batch_data.y).sum().item()
             val_acc = correct / len(Batch_data.y)
-            Acc_all, Acc1, Prec1, Recll1, F1, Acc2, Prec2, Recll2, F2, Acc3, Prec3, Recll3, F3, Acc4, Prec4, Recll4, F4 = evaluation4class(
+            Acc_all, Acc1, Prec1, Recll1, F1, Acc2, Prec2, Recll2, F2, Acc3, Prec3, Recll3, F3, Acc4, Prec4, Recll4, F4, rawdict = evaluation4class(
                 val_pred, Batch_data.y)
             temp_val_Acc_all.append(Acc_all), temp_val_Acc1.append(Acc1), temp_val_Prec1.append(
                 Prec1), temp_val_Recll1.append(Recll1), temp_val_F1.append(F1), \
@@ -95,6 +100,9 @@ def train_model(treeDic, x_test, x_train, args, iter):
             temp_val_Acc4.append(Acc4), temp_val_Prec4.append(Prec4), temp_val_Recll4.append(
                 Recll4), temp_val_F4.append(F4)
             temp_val_accs.append(val_acc)
+            for classy in rawdict:
+                for resultant_type in rawdict[classy]:
+                    rawcounts[classy][resultant_type] = rawdict[classy][resultant_type] + rawcounts[classy][resultant_type]
         val_losses.append(np.mean(temp_val_losses))
         val_accs.append(np.mean(temp_val_accs))
         print("Epoch {:05d} | Val_Loss {:.4f}| Val_Accuracy {:.4f}".format(epoch, np.mean(temp_val_losses),
@@ -110,6 +118,11 @@ def train_model(treeDic, x_test, x_train, args, iter):
                'C4:{:.4f},{:.4f},{:.4f},{:.4f}'.format(np.mean(temp_val_Acc4), np.mean(temp_val_Prec4),
                                                        np.mean(temp_val_Recll4), np.mean(temp_val_F4))]
         print('results:', res)
+        print(commentary,'results:', res)
+        print(commentary,"rawcounts:")
+        pprint.pprint(rawcounts)
+        
+        
         early_stopping(np.mean(temp_val_losses), np.mean(temp_val_accs), np.mean(temp_val_F1), np.mean(temp_val_F2),
                        np.mean(temp_val_F3), np.mean(temp_val_F4), model, 'BiGCN', args.datasetname)
         accs = np.mean(temp_val_accs)
@@ -125,7 +138,7 @@ def train_model(treeDic, x_test, x_train, args, iter):
             F3 = early_stopping.F3
             F4 = early_stopping.F4
             break
-    return accs, F1, F2, F3, F4
+    return train_losses , val_losses ,train_accs, val_accs,accs,F1,F2,F3,F4
 
 
 def init_seeds(seed=2020):
@@ -155,7 +168,7 @@ if __name__ == '__main__':
                         help='dimension of output features')
     parser.add_argument('--num_class', type=int, default=4, metavar='numclass',
                         help='number of classes')
-    parser.add_argument('--num_workers', type=int, default=30, metavar='num_workers',
+    parser.add_argument('--num_workers', type=int, default=0, metavar='num_workers',
                         help='number of workers for training')
 
     # Parameters for training the model
@@ -201,7 +214,11 @@ if __name__ == '__main__':
     parser.add_argument('--edge_num', type=int, default=2, metavar='edgenum', help='latent relation types T in the edge inference')
 
     args = parser.parse_args()
-
+    if args.datasetname.lower()=="pheme":
+        args.input_features=768*256 # bert shape.
+        args.batchsize=12 # bert kinda thing.
+        
+        
     if not args.no_cuda:
         print('Running on GPU:{}'.format(args.num_cuda))
         args.device = torch.device('cuda:{}'.format(args.num_cuda) if torch.cuda.is_available() else 'cpu')
@@ -213,48 +230,75 @@ if __name__ == '__main__':
     init_seeds(seed=args.seed)
 
     total_accs, total_NR_F1, total_FR_F1, total_TR_F1, total_UR_F1 = [], [], [], [], []
-    treeDic = loadTree(args.datasetname)
+    if args.datasetname.lower()=="pheme":
+        treeDic={}
+    else:
+        treeDic = loadTree(args.datasetname)
+    if args.datasetname.lower()!="pheme":
+        for iter in range(args.iterations):
+            iter_timestamp = time()
+            # fold_tests, fold_trains = load5foldData(args.datasetname)
+            fold_tests, fold_trains = load5foldData(args.datasetname, seed=args.seed)
 
-    for iter in range(args.iterations):
-        iter_timestamp = time()
-        # fold_tests, fold_trains = load5foldData(args.datasetname)
-        fold_tests, fold_trains = load5foldData(args.datasetname, seed=args.seed)
+            accs, NR_F1, FR_F1, TR_F1, UR_F1 = [], [], [], [], []
+            for fold_idx in range(5):
+                fold_timestamp = time()
+                _, __, ___, ____, acc, F1, F2, F3, F4 = train_model(treeDic, fold_tests[fold_idx], fold_trains[fold_idx], args, iter)
+                accs.append(acc)
+                NR_F1.append(F1)
+                FR_F1.append(F2)
+                TR_F1.append(F3)
+                UR_F1.append(F4)
 
-        accs, NR_F1, FR_F1, TR_F1, UR_F1 = [], [], [], [], []
-        for fold_idx in range(5):
-            fold_timestamp = time()
-            acc, F1, F2, F3, F4 = train_model(treeDic, fold_tests[fold_idx], fold_trains[fold_idx], args, iter)
-            accs.append(acc)
-            NR_F1.append(F1)
-            FR_F1.append(F2)
-            TR_F1.append(F3)
-            UR_F1.append(F4)
+                print("Iter:{}/{}\tFold:{}/5 - Acc:{:.4f}\tNR_F1:{:.4f}\tFR_F1:{:.4f}\tTR_F1:{:.4f}\tUR_F1:{:.4f}\tTime:{:.4f}s".format(
+                        iter, args.iterations,
+                        fold_idx,
+                        acc, F1, F2, F3, F4,
+                        time() - fold_timestamp))
 
-            print("Iter:{}/{}\tFold:{}/5 - Acc:{:.4f}\tNR_F1:{:.4f}\tFR_F1:{:.4f}\tTR_F1:{:.4f}\tUR_F1:{:.4f}\tTime:{:.4f}s".format(
-                    iter, args.iterations,
-                    fold_idx,
-                    acc, F1, F2, F3, F4,
-                    time() - fold_timestamp))
+            total_accs.append(np.mean(accs))
+            total_NR_F1.append(np.mean(NR_F1))
+            total_FR_F1.append(np.mean(FR_F1))
+            total_TR_F1.append(np.mean(TR_F1))
+            total_UR_F1.append(np.mean(UR_F1))
 
-        total_accs.append(np.mean(accs))
-        total_NR_F1.append(np.mean(NR_F1))
-        total_FR_F1.append(np.mean(FR_F1))
-        total_TR_F1.append(np.mean(TR_F1))
-        total_UR_F1.append(np.mean(UR_F1))
+            print("****  Iteration Result {}/{} Time:{:.4f}s  ****".format(iter, args.iterations, time() - iter_timestamp))
+            print("Acc:{:.4f}\tNR_F1:{:.4f}\tFR_F1:{:.4f}\tTR_F1:{:.4f}\tUR_F1:{:.4f}\t\tavg_F1:{:.4f}".format(np.mean(accs),
+                                                                                                             np.mean(NR_F1),
+                                                                                                             np.mean(FR_F1),
+                                                                                                             np.mean(TR_F1),
+                                                                                                             np.mean(UR_F1),
+                                                                                                             (np.mean(NR_F1) + np.mean(FR_F1) + np.mean(TR_F1) + np.mean(UR_F1)) / 4))
 
-        print("****  Iteration Result {}/{} Time:{:.4f}s  ****".format(iter, args.iterations, time() - iter_timestamp))
-        print("Acc:{:.4f}\tNR_F1:{:.4f}\tFR_F1:{:.4f}\tTR_F1:{:.4f}\tUR_F1:{:.4f}\t\tavg_F1:{:.4f}".format(np.mean(accs),
-                                                                                                         np.mean(NR_F1),
-                                                                                                         np.mean(FR_F1),
-                                                                                                         np.mean(TR_F1),
-                                                                                                         np.mean(UR_F1),
-                                                                                                         (np.mean(NR_F1) + np.mean(FR_F1) + np.mean(TR_F1) + np.mean(UR_F1)) / 4))
+        print("****  Total Result  ****")
+        print("Acc:{:.4f}\tNR_F1:{:.4f}\tFR_F1:{:.4f}\tTR_F1:{:.4f}\tUR_F1:{:.4f}".format(np.mean(total_accs),
+                                                                                          np.mean(total_NR_F1),
+                                                                                          np.mean(total_FR_F1),
+                                                                                          np.mean(total_TR_F1),
+                                                                                          np.mean(total_UR_F1)))
 
-    print("****  Total Result  ****")
-    print("Acc:{:.4f}\tNR_F1:{:.4f}\tFR_F1:{:.4f}\tTR_F1:{:.4f}\tUR_F1:{:.4f}".format(np.mean(total_accs),
-                                                                                      np.mean(total_NR_F1),
-                                                                                      np.mean(total_FR_F1),
-                                                                                      np.mean(total_TR_F1),
-                                                                                      np.mean(total_UR_F1)))
 
+    else:
+        with open("Eventsplit_details.txt","r") as eventsplitfile:
+            eventsplits = json.load(eventsplitfile)
+        treeDic = {} # Won't be using this...
+        for event in eventsplits:
+            print("-"*25,event,"-"*25)
+            testfold = eventsplits[event]
+            trainfold = []
+            for notevent in eventsplits:
+                if notevent==event:
+                    continue
+                trainfold.extend(eventsplits[notevent])
+            # for iter in range(iterations):
+            train_losses, val_losses, train_accs, val_accs0, accs0, F1_0, F2_0, F3_0, F4_0 = train_model(treeDic, testfold, trainfold, args, 0,commentary=event)
+            test_accs.append(accs0)
+            NR_F1.append(F1_0)
+            FR_F1.append(F2_0)
+            TR_F1.append(F3_0)
+            UR_F1.append(F4_0)
+            print("LOOP COMPLETED: EVENT- ",event)
+        print("OVERALL RESULTS FOR :",event)
+        print("Total_Test_Accuracy: {:.4f}|NR F1: {:.4f}|FR F1: {:.4f}|TR F1: {:.4f}|UR F1: {:.4f}".format(
+        sum(test_accs) / iterations, sum(NR_F1) /iterations, sum(FR_F1) /iterations, sum(TR_F1) / iterations, sum(UR_F1) / iterations))
 